b753acde1ea9ad57043b281369c4cacb5b62f0aa
DanielMawioo <mawioodaniel43@gmail.com>
2021-01-27 05:58:12 -0500

    update

--- NGO_NAIROBI/settings.py
+++ NGO_NAIROBI/settings.py
@@ -137,6 +137,7 @@ USE_TZ = True
 django_heroku.settings(locals())
 STATIC_URL = '/static/'#location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'NGO_NAIROBI/static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
