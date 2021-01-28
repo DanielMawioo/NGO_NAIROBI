c38b35e725b52bd8811e12fb27db9c8817460c89
DanielMawioo <mawioodaniel43@gmail.com>
2021-01-28 08:00:42 -0500

    commit

--- NGO_NAIROBI/settings.py
+++ NGO_NAIROBI/settings.py
@@ -137,10 +137,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'#location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')# location where you will store your static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 MEDIA_URL = '/media/'
 
 LEAFLET_CONFIG = {
